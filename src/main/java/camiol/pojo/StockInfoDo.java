f75cd6bc5a43358a4faf0e740abdce2d08cc32f6
Camiol <camioljoyce@gmail.com>
2020-09-30 17:38:20 +0800

    新建mybatis generator 搭配mysql建pojo和mapper

---
+++ src/main/java/camiol/pojo/StockInfoDo.java
@@ -0,0 +1,45 @@
+package camiol.pojo;
+
+import java.util.Date;
+
+public class StockInfoDo {
+    private Integer stockId;
+
+    private String stockName;
+
+    private Date lastUpdateTime;
+
+    private Date createTime;
+
+    public Integer getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(Integer stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ src/main/java/camiol/pojo/StockPriceDo.java
@@ -0,0 +1,75 @@
+package camiol.pojo;
+
+import java.util.Date;
+
+public class StockPriceDo {
+    private Integer stockId;
+
+    private String tranDate;
+
+    private String tranTime;
+
+    private Integer price;
+
+    private Integer amount;
+
+    private Date lastUpdateTime;
+
+    private Date createTime;
+
+    public Integer getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(Integer stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getTranDate() {
+        return tranDate;
+    }
+
+    public void setTranDate(String tranDate) {
+        this.tranDate = tranDate;
+    }
+
+    public String getTranTime() {
+        return tranTime;
+    }
+
+    public void setTranTime(String tranTime) {
+        this.tranTime = tranTime;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
